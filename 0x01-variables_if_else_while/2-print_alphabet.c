b522327e26e8d05e1721ecfde8139446953535be
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-15 04:39:57 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,8 +11,8 @@
 int main(void)
 {
         char c = 'a';
-        while (c <= 'z')
-                putchar (c++);
-        putchar ('\n');
-        return (0);
+        while(c <= 'z')
+                putchar(c++);
+        putchar('\n');
+        return(0);
 }

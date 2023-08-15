d0dad2bba2874c721ce7f0d44cd3383cce9bd896
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-15 05:12:49 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: false or true vailidation
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+        int c = 48;
+        while(c <= 57)
+          {
+                putchar(c++);
+            if(c<=57)
+            {
+              putchar(',');
+              putchar(' ');
+            {
+          }
+        return(0);
+}

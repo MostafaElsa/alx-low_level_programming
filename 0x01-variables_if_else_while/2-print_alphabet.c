8be028b90cc1da1ffbc0c4e50bc53df5c7cbc4a7
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-15 04:23:52 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* main - entry*/
+/* check if n is postive or negative*/
+int main(void)
+{
+        char c = 'a';
+        while(c<='z')
+            putchar(ch++);
+        putchar(/n);
+        return(0);
+}
 

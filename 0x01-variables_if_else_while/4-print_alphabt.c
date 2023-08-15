d3d916162f1dadfb3af0ad40f06ca6dbb97f13ee
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-15 04:44:43 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: false or true vailidation
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+        char c = 'a';
+        while(c <= 'z')
+        {
+          if (c !=q )
+            putchar(c++);
+          else
+            continue;
+          }
+        putchar('\n');
+        return(0);
+}

fa833995425524d4ddb25077ce274c1f45bba348
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-17 15:01:52 -0700

    seventh

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print size of data types
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Size of a char: %u byte(s)", sizeof(char));
+	printf("Size of an int: %u byte(s)", sizeof(int));
+	printf("Size of a long int: %u byte(s)", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)", sizeof(long long int));
+	printf("Size of a float: %u byte(s)", sizeof(float));
+	return (0);
+}

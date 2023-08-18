458148e5a68f43d670e0916abb7c3e63920833d7
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-18 14:35:38 -0700

    0x04

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Function
+ *
+ * @a: first num
+ * @b: second num
+ *
+ * Description: print putchar
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}

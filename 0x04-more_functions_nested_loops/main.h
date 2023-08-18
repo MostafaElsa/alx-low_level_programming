187121c68a09ecb3fd90b7d87c63cd9056d521c2
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-18 14:56:04 -0700

    0x04

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _putchar(char c);
 void print_triangle(int size);
 void print_square(int size);
 void print_diagonal(int n);

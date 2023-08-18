96adaa81a73a04b2ce6c38d46a4b20e625fabaa7
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-18 12:29:51 -0700

    0x04

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: print putchar
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97 ; i < 123 ; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

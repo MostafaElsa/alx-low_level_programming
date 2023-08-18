8211b5daf6e73a3ccb86038db28bb4a2defea09c
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-18 13:28:24 -0700

    0x04

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Function
+ * @n: int
+ * Description: print putchar
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}

d9e005eea1750141c134a08574535228eb7596da
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-18 12:50:51 -0700

    0x04

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	
+
 	for (i = 0 ; i < 10 ; i++)
 	{
 		int j;
-		
+
 		for (j = 97 ; j < 123 ; j++)
 		{
-			_putchar(i);
-		}	
-		_putchar('\n');	
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
 }

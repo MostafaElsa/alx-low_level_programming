8617b194c412a5c5a2ae9ff7f63f9d06846378c0
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-17 19:53:21 -0700

    no

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Function
+ *
+ * Description: whether char is upper or lower case
+ *
+ * Return: 1 (upper) 0 (lower)
+*/
+
+void print_most_numbers(void)
+{
+	for (int i = 48 : i < 58 ; i++)
+	{
+		putchar(i);
+		if (i == 50 || i == 52)
+			continue;
+	}
+	putchar('\n');
+}

e8f068d64fb7d09779d4f49d46bf81a3887a560c
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-18 08:55:07 -0700

    0x04

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Function
+ * @n: int
+ * Description: whether char is upper or lower case
+ *
+ * Return: 1 (upper) 0 (lower)
+*/
+
+void print_line(int n)
+{
+	for (int i = 0 : i < n ; i++)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}

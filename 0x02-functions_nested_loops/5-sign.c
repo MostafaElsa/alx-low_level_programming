5893f0217b3fdd9e07aa33d3450a3a6dd284fabc
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-18 13:08:58 -0700

    0x04

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - Function
+ * @n: int
+ * Description: print putchar
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (0);
+}

544999a9bfa88dc533d0f06da8ab677b8a9b49d5
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-18 18:21:03 -0700

    0x04

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,19 +15,17 @@ void more_numbers(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		int j;
+		int j, y;
 
 		for (j = 0 ; j <= 14 ; j++)
 		{
-			if (j < 10)
-				_putchar(48 + j);
-			else
+			y = j;
+			if (j >= 10)
 			{
-				int y = j % 10;
-
+				y = j % 10;
 				_putchar(48 + 1);
-				_putchar(48 + y);
 			}
+			_putchar(48 + y);
 		}
 		_putchar('\n');
 	}

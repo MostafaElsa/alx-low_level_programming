8a993acbd3c619553d531724589144f7c5bd0a78
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-19 14:41:10 -0700

    g

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: false or true vailidation
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i + 48);
+			putchar(j + 48);
+			counter++;
+			if ((counter == 2) && (i != 9 || j != 9))
+			{
+				putchar(',');
+				putchar(' ');
+				counter = 0;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

42def022f051e353f48dd036fb5b1af110e2ee2b
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-19 12:12:01 -0700

    s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int i;
 
 	i = 0;

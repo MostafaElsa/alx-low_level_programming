86dd58a98d84116af15b6842b2d203875f072700
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-19 19:59:16 -0700

    j

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print putchar
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+*/
+int main(void)
+{
+	int a = 1, b = 2, res;
+
+	for (i = 0 ; i < 50 ; i++)
+	{
+		if (i != 49)
+			printf("%d, ", a);
+		else
+			printf("%d", a);
+		res = a + b;
+		a = b;
+		b = res;
+	}
+	return (0);
+}

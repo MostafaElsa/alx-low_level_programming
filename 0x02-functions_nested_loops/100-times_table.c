27e350b31e1515ff0bb96fbba8f6bfda495b282d
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-20 04:30:03 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,15 +23,39 @@ void print_times_table(int n)
 			{
 				int x = (i * j) + 48;
 
-				_putchar(x);
-				if (j != n)
+				if(j == 0)
 				{
-					_putchar(',');
+					_putchar('0');
+					continue;
+				}
+				if (x >= 100)
+				{
+					int a = (x / 100);
+
+					_putchar(' ');
+					_putchar(a + '0');
+					x -= (a * 100);
+					a = (x / 10);
+					_putchar(a + '0');
+					x -= (a * 10);
+					_putchar(x + '0');
+				}
+				if else (x >= 10)
+				{
 					_putchar(' ');
 					_putchar(' ');
+					int a = (x / 10);
+
+					_putchar(a + '0');
+					x -= (a * 10);
+					_putchar(x + '0');
 				}
 				else
-					_putchar('\n');
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(x + '0');
+				}
 			}
 		}
 	}

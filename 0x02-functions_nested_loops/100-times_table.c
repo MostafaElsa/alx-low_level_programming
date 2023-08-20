4b392bb7e8e25e80b12775ff12487d0fc20a9abc
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-19 17:39:37 -0700

    e

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_times_table - Function
+ *
+ * @n: int
+ * Description: print putchar
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+*/
+void print_times_table(int n)
+{
+	int i;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		int j;
+
+		for (j = 0 ; j <= n ; j++)
+		{
+			int x = i * j;
+
+			_putchar(x);
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
+		}
+	}
+}

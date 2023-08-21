7524022ae7bd228c487cc2a5543a431d434f6624
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-21 15:20:25 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - Function
+ *
+ * @str: pointer
+ * Description: whether char is upper or not
+ *
+ * Return: 1 (digit) 0 (not)
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		putchar(*(s + i));
+		i++;
+	}
+}

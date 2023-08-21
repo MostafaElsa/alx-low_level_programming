93cc71332c9f712f6ac7075dfae3b2a8e67473c2
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-21 16:44:03 -0700

    0x05

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,18 +14,17 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n > 0)
 	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", a[i]);
+			if (i != n - 1)
+				printf(", ");
+			else
+				printf("\n");
+		}
+	}
+	else
 		printf("\n");
-		break;
-	}
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", a[i]);
-		if (i != n - 1)
-			printf(", ");
-		else
-			printf("\n");
-	}
-
 }

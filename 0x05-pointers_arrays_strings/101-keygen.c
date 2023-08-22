ebb812d070979d558f817343bd9632eabeb74c1a
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-21 17:22:23 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Function
+ *
+ * Description: whether char is upper or not
+ *
+ * Return: 1 (digit) 0 (not)
+*/
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2745)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}

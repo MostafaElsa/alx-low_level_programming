e1879fae0e4ee898e7e01eb7985a715df8d2abe6
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-24 23:12:47 +0000

    0x06

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,21 +11,35 @@
 
 char *leet(char *a)
 {
-	int i = 0;
+	int i = -1;
 
-	while (a[i] != '\0')
+	while (a[++i] != '\0')
 	{
 		if (a[i] == 'A' || a[i] == 'a')
+		{
 			a[i] = 52;
-		else if (a[i] == 'e' || a[i] == 'E')
+			continue;
+		}
+		if (a[i] == 'e' || a[i] == 'E')
+		{
 			a[i] = 51;
-		else if (a[i] == 'O' || a[i] == 'o')
+			continue;
+		}
+		if (a[i] == 'O' || a[i] == 'o')
+		{
 			a[i] = 48;
-		else if (a[i] == 'T' || a[i] == 't')
+			continue;
+		}
+		if (a[i] == 'T' || a[i] == 't')
+		{
 			a[i] = 55;
-		else if (a[i] == 'l' || a[i] == 'L')
+			continue;
+		}
+		if (a[i] == 'l' || a[i] == 'L')
+		{
 			a[i] = 49;
-		i++;
+			continue;
+		}
 	}
 	a[i] = '\0';
 	return (a);

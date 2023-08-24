3cbc13d5a4b5b4e6528d24095c203d09b00e1670
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-24 21:47:21 +0000

    0x06

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - check the code
+ *
+ * @a: array
+ * @n: number of elements
+ * Return: Always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int j = 0;
+
+	while (j < n / 2)
+	{
+		a[i] = a[n - 1 - i];
+		i++;
+	}
+}

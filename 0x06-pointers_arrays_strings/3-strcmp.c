b87a75803f35b9cbc1a114ab34cebb74f539e4ca
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-24 21:25:38 +0000

    0x06

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcmp - check the code
+ *
+ * @s1: dest
+ * @s2: src
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	do {
+		if (s1[j] - s2[j] > 0)
+			return (s1[j] - s2[j]);
+		else if (s1[j] - s2[j] > 0)
+			return (s1[j] - s2[j]);
+		else if (s1[j] - s2[j] == 0)
+		{
+			if (s1[j] == '\0')
+				return (0);
+			j++;
+		}
+	} while (1);
+}
+

c50cce9b556d6ffa7dbe242815ddfbe7aafbed29
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-24 22:41:27 +0000

    0x06

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *cap_string - check the code
+ *
+ * @a: array
+ *
+ * Return: Always 0.
+ */
+
+char *cap_string(char *a)
+{
+	int i = 0;
+
+	if (a[i] >= 'a' && a[i] <= 'z')
+		a[i] -= 32;
+	i++;
+	while (a[i] != '\0')
+	{
+		if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n' || a[i] == ',')
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+				a[i + 1] -= 32;
+		}
+		else if (a[i] == ';' || a[i] == '.' || a[i] == '!')
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+			a[i + 1] -= 32;
+		}
+		else if (a[i] == ')' || a[i] == '{' || a[i] == '}')
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+			a[i + 1] -= 32;
+		}
+		else if (a[i] == '?' || a[i] == '"' || a[i] == '(')
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+				a[i + 1] -= 32;
+		}
+		i += 2;
+	}
+	a[i] = '\0';
+	return (a);
+}

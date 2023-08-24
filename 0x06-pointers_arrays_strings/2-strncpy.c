5fdea6292f1c938abb4a5eff0b172c9adcafe795
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-24 23:20:15 +0000

    0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,8 +18,12 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		if (src[j] == '\0')
 		{
-			dest[j] = src[j];
-			break;
+			while (j < n)
+			{
+				dest[j] = '\0';
+				j++;
+			}
+			return (dest);
 		}
 		dest[j] = src[j];
 		j++;

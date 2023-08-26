3487f4a31980ffa1981cbdf155193e35caad2b1a
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-26 00:09:46 +0000

    0x06

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,46 +10,46 @@
  *
  * Return: 1 (upper) 0 (lower)
 */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	unsigned int i = 0, j = 0, k, len_n1 = 0, len_n2 = 0, max;
+	int i = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	short x = 0;
-
-	while (n1[i++] != '\0')
-		len_n1++;
-	while (n2[j++] != '\0')
-		len_n2++;
-	if (len_n1 >= len_n2)
-		max = len_n1;
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	if (i >= j)
+		max = i;
 	else
-		max = len_n2;
-	if (max  >= size_r)
+		max = j;
+	if (size_r <= max + 1)
 		return (0);
-	r[size_r - 1] = '\0';
-	i = size_r - 2;
-	j = len_n1 - 1;
-	k = len_n2 - 1;
-	while (j != 0 && k != 0)
+	r[max + 1] = '\0';
+	i--, j--;
+	res1 = *(n1 + i) - 48, res2 = *(n2 + j) - 48;
+	while (max >= 0)
 	{
-		if (n1[j] + n2[k] > 105)
-		{
-			r[i--] = (x + n1[j--] + n2[k--]) - 106;
+		res = res1 + res2 + x;
+		if (res >= 10)
 			x = 1;
-		}
 		else
-		{
-			r[i--] = (x + n1[j--] + n2[k--]);
 			x = 0;
-		}
+		if (res > 0)
+		*(r + max) = (sum - 10) + 48;
+		else
+			*(r + max) = '0';
+		if (i > 0)
+			i--, res1 = *(n1 + i) - 48;
+		else
+			res1 = 0;
+		if (j > 0)
+			j--, res2 = *(n2 + j) - 48;
+		else
+			res2 = 0;
+		max--;
 	}
-	while (j != 0)
-		r[i--] = n1[j--];
-	while (k != 0)
-		r[i--] = n2[k--];
-	if (x == 1)
-		return (0);
+	if (*(r) == '0')
+		return (r + 1);
 	else
 		return (r);
 }

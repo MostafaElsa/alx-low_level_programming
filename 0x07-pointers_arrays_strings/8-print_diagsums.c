9808251b9990a450301c3851dcf44b34016f848c
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-28 23:51:19 +0000

    0x07

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  * print_diagsums - function
+ *  @a: int pointer to array 2D
+ *  @size: size of
+ *  Return: Nothing.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, x = 0, y = size - 1, sum1 = 0, sum2 = 0;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		sum1 += *(a + (size * i));
+	}
+	a += (size - 1);
+	for (i = 0 ; i < size ; i++)
+	{
+		sum2 += *(a + ((size - 1) * i));
+	}
+	printf("%d, %d", sum1, sum2);
+}

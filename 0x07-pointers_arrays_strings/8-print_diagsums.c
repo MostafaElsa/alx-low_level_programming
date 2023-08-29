7302fdbc219327697d9222b06cdac8f2b164fb88
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-29 00:06:46 +0000

    0x07

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, x = 0, y = size - 1, sum1 = 0, sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0 ; i < size ; i++)
 	{
@@ -20,5 +20,5 @@ void print_diagsums(int *a, int size)
 	{
 		sum2 += *(a + ((size - 1) * i));
 	}
-	printf("%d, %d", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,5 +12,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-#endif // MAIN_H
+#endif
 

7a603869eab3e6b63ce1f945eae17e6516046ee4
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-29 01:17:10 +0000

    0x07

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  * set_string - function
+ *  @s: string to copy in
+ *  @to: string to copy from
+ *  Return: Nothing.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

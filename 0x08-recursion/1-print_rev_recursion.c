d53c09697c04a82281a7edd0096d3a24a5609430
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-08-30 11:24:55 +0000

    0x08

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- 		_print_rev_recursion - check the code
+ *	_print_rev_recursion - check the code
  *	@s: string to print
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
-	{
-		s++;
-		_print_rev_recursion(s);
-	}
-	_putchar(*s);
+		_print_rev_recursion(s + 1);
+	if (*s != '\0')
+		_putchar(*s);
 }

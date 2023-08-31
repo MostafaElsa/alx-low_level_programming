63456fbadf7c22a321ab4bcdb89d43497ead15ac
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-31 04:09:24 +0300

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *	is_palindrome - check the code
+ *	@s: string
+ *	Return: result
+ */
+int is_palindrome(char *s)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	{
+		y--;
+		return (x * _pow_recursion(x, y));
+	}
+}

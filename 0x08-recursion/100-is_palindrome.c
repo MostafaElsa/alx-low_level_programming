911b35bf465304fd02de08a3917db3d9eb2b83f5
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-31 05:53:10 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,6 @@
 #include "main.h"
 
 /**
- *	is_palindrome - check the code
- *	@s: string
- *	Return: result
- */
-int is_palindrome(char *s)
-{
-    int i = _strlen_recursion(s) - 1;
-	return (_palindrome(0, i, s)
-}
-/**
  *	_palindrome - check the code
  *	@i: number1
  *	@j: number2
@@ -19,9 +9,20 @@ int is_palindrome(char *s)
  */
 int _palindrome(int i, int j, char *s)
 {
-    if (s[i] != s[j])
-        return (0);
-    if (i >= j)
-        return (1);
-    return (_palindrome(++i, --j, s)
+	if (s[i] != s[j])
+		return (0);
+	if (i >= j)
+		return (1);
+	return (_palindrome(++i, --j, s));
+}
+
+/**
+ *	is_palindrome - check the code
+ *	@s: string
+ *	Return: result
+ */
+int is_palindrome(char *s)
+{
+	int i = _strlen_recursion(s) - 1;
+	return (_palindrome(0, i, s)
 }

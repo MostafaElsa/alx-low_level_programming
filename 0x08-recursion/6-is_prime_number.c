82713738cc94a69e0f1de017b5515fac60d1a92e
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-31 05:48:09 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ *	_prime - check the code
+ *	@n: number
+  *	@r: root
+ *	Return: result
+ */
+int _prime(int n, int r)
+{
+    if (r < n && n % r == 0)
+        return (0);
+    if (r = n)
+        return (1);
+    return (n, ++r);
+}
+
+/**
  *	is_prime_number - check the code
  *	@n: number
  *	Return: result
@@ -10,12 +25,4 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 	return (_prime(n, 2);
-}
-int _prime(int n, int r)
-{
-    if (r < n && n % r == 0)
-        return (0);
-    if (r = n)
-        return (1);
-    return (n, ++r);
 }

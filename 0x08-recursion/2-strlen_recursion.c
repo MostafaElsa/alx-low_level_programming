8fb329db456a46ed0ebcb29d2f573802048c3482
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-31 04:53:50 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- *	_print_rev_recursion - check the code
+ *	_strlen_recursion - check the code
  *	@s: string to print
  */
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
-		_print_rev_recursion(s + 1);
-	if (*s != '\0')
-		_putchar(*s);
+        return(1 + _strlen_recursion(s++);
 }

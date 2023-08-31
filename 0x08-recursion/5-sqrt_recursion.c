91a8bbe72730b1dcb6cda76606652d32d0ef8003
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-31 04:05:54 +0300

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *	_sqrt_recursion - check the code
+ *	@n: number
+ *	Return: result
+ */
+int _sqrt_recursion(int n)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	{
+		y--;
+		return (x * _pow_recursion(x, y));
+	}
+}

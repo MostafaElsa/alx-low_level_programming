14f5c476684c73a4821ef627bc4941fe54304836
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-31 04:07:35 +0300

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *	is_prime_number - check the code
+ *	@n: number
+ *	Return: result
+ */
+int is_prime_number(int n)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	{
+		y--;
+		return (x * _pow_recursion(x, y));
+	}
+}

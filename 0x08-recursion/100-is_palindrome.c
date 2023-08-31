236575796648f55e7b7cfbf0cf2e2b99e27b43e7
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-08-31 05:15:50 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,12 +7,21 @@
  */
 int is_palindrome(char *s)
 {
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	{
-		y--;
-		return (x * _pow_recursion(x, y));
-	}
+    int i = _strlen_recursion(s) - 1;
+	return (_palindrome(0, i, s)
+}
+/**
+ *	_palindrome - check the code
+ *	@i: number1
+ *	@j: number2
+ *	@s: string
+ *	Return: result
+ */
+int _palindrome(int i, int j, char *s)
+{
+    if (s[i] != s[j])
+        return (0);
+    if (i >= j)
+        return (1);
+    return (_palindrome(++i, --j, s)
 }

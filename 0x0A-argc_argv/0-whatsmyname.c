8f22391c1cbf46ac42fd30678720cf21de0520bf
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-01 21:03:35 +0000

    0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print with printf function
+ *
+ * Return: Always 0 (Success)
+*/
+int main(int argc,char **argv)
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}

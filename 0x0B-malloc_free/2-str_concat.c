4885e7c87e358445fc3bbbe830df5a300179e16f
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-05 18:38:17 +0000

    0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 
 	while (s1 != NULL && s1[i] != '\0')
 		i++;
-	while (s2 != NULL && (s2[j] != '\0')
+	while (s2 != NULL && s2[j] != '\0')
 		j++;
 	ptr = (char *)malloc(i + j + 1);
 	if (ptr == NULL)

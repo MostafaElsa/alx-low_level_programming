604d5e8b900d02f29dbcc81ef5c6d15e335fb933
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-09-07 01:46:04 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,30 +1,39 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * alloc_grid - create 2D array initialized to 0
- * @width: the 1st Dim
- * @height: the 2nd Dim
- * Return: pointer to 2Dim array
- */
+* alloc_grid - function that returns a pointer to 2D array of int
+* @width: number of columns
+* @height: number of rows
+* Return: return a pointer to the array otherwise return NULL
+*/
+
 int **alloc_grid(int width, int height)
 {
-	int i = 0, j = 0, *ptr1, **ptr2;
+	int **grid, i;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr1 = (int*)malloc(width * sizeof(int));
-	ptr2 = (int **)malloc(height * sizeof(ptr));
-	if (ptr1 == NULL || ptr2 == NULL)
+
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
 		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < width; j++)
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
 		{
-			*ptr2[j] = 0;
-			ptr2++;
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
 		}
 	}
-	return (ptr2);
+	return (grid);
 }

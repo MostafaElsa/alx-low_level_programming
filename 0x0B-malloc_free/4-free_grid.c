e4bb3e522deb1d6dc456f4c82c107356a5e3ad2f
MostafaElsa <138707911+MostafaElsa@users.noreply.github.com>
2023-09-07 01:51:58 +0300

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
+/**
+* free_grid - function that free 2D array of int
+* @grid: pointer to pointers of 1D array of int
+* @height: number of rows
+*/
+
+void free_grid(int **grid, int height)
+{
+  int i;
+
+  for (i = 0; i < height; i++)
+  {
+    free (grid[i]);
+  }
+  free (grid);
+}

4680972ab8f61c592f4bc61fba461ef5b60216fc
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-06 23:22:57 +0000

    0x0B

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,32 +6,34 @@
 * argstostr - function that  that concatenates all the arguments
 * @ac: number of arguments
 * @av: arguments
+* Return: pointer to result arguments
 */
 
 char *argstostr(int ac, char **av)
 {
-  int i, **ptr;
+	int i, **ptr;
 
-  ptr = (char **)malloc(sizeof(char *) * ac)
-  if (av == NULL)
-    return (NULL);
-  for (i = 0; i < ac; i++)
-  {
-    ptr[i] = (int *)malloc(sizeof(av[i]));
-    if (av[i] == null)
-    {
-      while (i >= 0)
-        {
-          free(av[i])
-            i--;
-        }
-      free(av);
-    }
-    else 
-    {
-      ptr[i] = av[i];
-      if (i != ac - 1)
-      ptr[i][sizeof(av[i]) - 1] = '\n';
-    }
-    return (ptr);
+	ptr = (char **)malloc(sizeof(char *) * ac);
+	if (av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		ptr[i] = (int *)malloc(sizeof(av[i]));
+		if (av[i] == null)
+		{
+			while (i >= 0)
+			{
+				free(av[i]);
+				i--;
+			}
+			free(av);
+		}
+	}
+	else
+	{
+		ptr[i] = av[i];
+		if (i != ac - 1)
+			ptr[i][sizeof(av[i]) - 1] = '\n';
+	}
+	return (ptr);
 }

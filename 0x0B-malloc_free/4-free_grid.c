f39a4ebaa61bb8d54353ec34edfd09e297dfb8b6
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-06 23:16:39 +0000

    0x09

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -14,9 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
-	ptr1 = (int *)malloc(sizeof(int) * width);
-	ptr2 = (int **)malloc(sizeof(ptr1) * height);
+	ptr = (int **)malloc(width * height);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)

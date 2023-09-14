0be5799d7d6f1f045a357491dc7a0ab42f2d2fbf
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-14 19:24:09 +0000

    0x0C

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,18 +2,23 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - function
+ * int_index - function
  * @array: int pionter
  * @size: size of array
- * @action: pointer to function
+ * @cmp: pointer to function
+ * Return: index of first element cmp != 0
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	size_t i;
+	int i;
 
+	if (size <= 0)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		(*action)(array[i]);
+		if ((*cmp)(array[i]) == 0)
+			return (i);
 	}
+	return (-1);
 }

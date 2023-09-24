69185e255745b50442592328958ee37ecd1f5924
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-24 11:55:20 +0000

    0x00

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 
 /**
  * main - entry point
+ *
  * Description: prints
+ *
  * Return: On success 1.
  * On error, -1 is returned, and error is set appropriately.
-*/
+ */
 int main(void)
 {
 	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";

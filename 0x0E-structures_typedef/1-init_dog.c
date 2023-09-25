e40265e7626280c6285ba9c8bcab0b67cca6dbcb
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-25 20:54:16 +0000

    0x0C

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,26 +8,28 @@
  * @name: name
  * @age: age
  * @owner: owner
- *
- * Return: Always 0.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	int i = 0;
+	int i = 0,len = 1;
 
+	while (name[i] != '\0')
+		len++;
+	d->name = malloc(len);
 	while (name[i] != '\0')
 	{
 		(d->name)[i] = name[i];
 		i++;
 	}
-	(d->name)[i] = '\0';
 	i = 0;
+	while (owner[i] != '\0')
+		len++;
+	d->owner = malloc(len);
 	while (owner[i] != '\0')
 	{
 		(d->owner)[i] = owner[i];
 		i++;
 	}
-	(d->owner)[i] = '\0';
 	d->age = age;
 }

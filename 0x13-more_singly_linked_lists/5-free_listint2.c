807e23c4101e4e2d9a0bac28448b705ddb814e8d
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-26 20:11:02 +0000

    0x12

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - functionn
+ *
+ * @head: pointer for pointer for head
+ */
+
+void free_listint2(listint_t *head)
+{
+	listint_t *temp;
+
+	head = NULL;
+}

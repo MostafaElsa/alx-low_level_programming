c6dd9dadd22cb01112ce8d191e96da7677846faa
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-26 14:43:42 +0000

    0x12

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - functionn
+ *
+ * @h: pointer for head
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+		n++;
+	return (n);
+
+}

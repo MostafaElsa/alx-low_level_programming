37ca6f6efee9c4ed03bae0a411f5c44e09af1ad3
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-26 19:13:17 +0000

    0x12

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - functionn
+ *
+ * @h: pointer for head
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		len++;
+	}
+	return (len);
+
+}

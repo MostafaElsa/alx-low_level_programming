aaaec9a5a77a3f5b1eec3532883e07cc80668a41
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-26 12:37:56 +0000

    0x0C

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *n, *o;
 
 	d = malloc(sizeof(dog_t));
+	n = malloc(sizeof(name));
 	d->name = malloc(sizeof(name));
+	o = malloc(sizeof(owner));
 	d->owner = malloc(sizeof(owner));
 	if (d->name == NULL || d->owner == NULL)
+	{
+		free(o);
+		free(n);
+		free(d);
 		return (NULL);
+	}
+	n = name;
+	o = owner;
 	d->name = name;
 	d->owner = owner;
 	d->age = age;

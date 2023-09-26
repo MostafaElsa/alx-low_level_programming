9b03f5124d587b204da4f649ee5aca57cd82af82
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-26 18:36:29 +0000

    0x12

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - functionn
+ *
+ * @head: pointer for pointer for head
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head->next)
+	{
+		temp = head->next;
+		free(head->next);
+		free(head->str);
+		head = temp
+	}
+	free(head);
+	free(temp);
+}

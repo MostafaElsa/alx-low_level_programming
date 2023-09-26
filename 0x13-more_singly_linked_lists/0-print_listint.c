96ee7723f4078ae7d3f807dc06f71430b64b6e12
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-09-26 19:15:36 +0000

    0x12

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,16 @@
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t print_list(const listint_t *h)
 {
-	size_t len = 0;
+	size_t n = 0;
 
 	while (h != NULL)
 	{
-		printf("%lu\n", h->n);
+		printf("%u", h->n);
 		h = h->next;
-		len++;
+		n++;
 	}
-	return (len);
+	return (n);
 
 }

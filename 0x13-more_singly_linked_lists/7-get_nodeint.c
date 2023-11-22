efc89965852e2694a362381f3eeb280562d12549
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-11-22 21:26:10 +0000

    0x12

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,8 +19,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (index)
 	{
 		temp = temp->next;
-		if(!temp)
-			return(NULL);
+		if (!temp)
+			return (NULL);
 		index--;
 	}
 	return (temp);

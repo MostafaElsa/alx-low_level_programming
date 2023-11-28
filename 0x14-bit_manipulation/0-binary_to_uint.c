14ec9a957391104e8e4a11455b83488d19505b56
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-11-28 19:05:58 +0000

    0x12

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - functionn
+ *
+ * @b: pointer to 0,1 chars
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned i = 0, x = 0, y = 1;
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			x = 0;
+			return (x);
+		}
+		i++;
+	}
+	while (i--)
+	{
+		x += ((b[i] - 48) * y);
+		y *= 2;
+	}
+	return (x);
+}
---
+++ 0x14-bit_manipulation/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - functionn
+ *
+ * @h: pointer for head
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+
+}

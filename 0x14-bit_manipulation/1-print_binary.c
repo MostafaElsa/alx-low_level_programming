81d95288c47cb1e2b78a9627a487890bdc93bf13
MostafaElsa <mostafaelhabashy41@gmail.com>
2023-11-29 16:05:11 +0000

    0x12

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,20 +12,22 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int i = 1, x = n;
 
+	if (n == 0)
+		printf("0");
 	while (i <= n)
-		i << 1;
-	i >> 1;
-	while (x != 0)
+		i <<= 1;
+	i >>= 1;
+	while (i != 0)
 	{
 		if (x >= i)
 		{
 			x -= i;
-			i >> 1;
+			i >>= 1;
 			printf("1");
 		}
 		else
 		{
-			i >> 1;
+			i >>= 1;
 			printf("0");
 		}
 	}
